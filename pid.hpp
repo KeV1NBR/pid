d7fa6cb93bb7362f57161c18d15515b3b91cb17f
Yu Xiong Wu <b1065144@mail.nuk.edu.tw>
2019-04-13 15:46:08 +0800

    add cfg

--- pid.hpp
+++ pid.hpp
@@ -1,11 +1,12 @@
 #pragma once
-
+#include <iostream>
 
 class PID
 {
 public:
     //member function
     PID(double Kp, double Ki, double Kd, double threshold);
+    PID(const std::string cfg);
     ~PID();
     void init(); //set error, preInput, integral to 0
     double* pidXY(double x, double y, double offset);  //pid in 2D help you to use easily
